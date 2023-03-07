f618ec48b5166181941a9cc5739fc5070b14bb3a
phuongmt3 <maituphuong3@gmail.com>
2023-03-07 13:46:30 +0700

    train and test for 100 articles

--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,6 @@
 from underthesea import pos_tag
 
-with open('scrapy_test/spiders/Output/newsContent.txt', 'r', encoding='utf-8') as f:
-    for line in f.readlines():
-        print(line)
-        print(pos_tag(line))
+string = '. Test .'
+print(string.strip())
 
 
--- scrapy_test/spiders/NewsCrawler.py
+++ scrapy_test/spiders/NewsCrawler.py
@@ -1,3 +1,5 @@
+import random
+
 import scrapy
 
 
@@ -11,21 +13,39 @@ class QuotesSpider(scrapy.Spider):
     def parse(self, response):
         if self.cnt >= self.pageLimit:
             return
-        with open('scrapy_test/spiders/Output/newsContent.txt', 'a+', encoding='utf-8') as f:
-            contents = response.css('.main-content ::text')
-            if contents and response.css('.bread-crumb-detail  a::attr(title)').get():
-                self.cnt += 1
-                f.write(str(self.cnt) + '-- ')
-                f.write(response.css('title::text').get() + '\n' +
-                        response.css('meta[name*=description]::attr(content)').get() + '\n')
+
+        contents = response.css('.main-content ::text')
+        category = response.css('.bread-crumb-detail  a::attr(title)').get()
+        keywords = response.css('meta[name*=keywords]::attr(content)').get()
+        title = response.css('title::text').get().strip()
+        if contents and response.css('.bread-crumb-detail  a::attr(title)').get() \
+                and category != 'Video' and category != 'Premium':
+            self.cnt += 1
+            if 60 < random.randint(0, 100) <= 80:
+                ftest = open('scrapy_test/spiders/Output/testSet.txt', 'a+', encoding='utf-8')
+                ftestans = open('scrapy_test/spiders/Output/testAns.txt', 'a+', encoding='utf-8')
+                ftest.write(title + ' ' + response.css('meta[name*=description]::attr(content)').get().strip() + ' ')
                 for p in contents:
-                    f.write(p.get().strip() + ' ')
-                f.write('\n')
+                    ftest.write(p.get().strip() + ' ')
+                ftest.write('\n')
+                ftestans.write(getSubjectName(category, keywords, title) + '\n')
+                ftest.close()
+                ftestans.close()
+
+            else:
+                ftrain = open('scrapy_test/spiders/Output/trainSet.txt', 'a+', encoding='utf-8')
+                ftrain.write(getSubjectName(category, keywords, title) + '  ')
+                ftrain.write(title + ' ' + response.css('meta[name*=description]::attr(content)').get().strip() + ' ')
+                for p in contents:
+                    ftrain.write(p.get().strip() + ' ')
+                ftrain.write('\n')
+                ftrain.close()
 
             yield {
                 'URL': response.url,
-                'Category': response.css('.bread-crumb-detail  a::attr(title)').get(),
-                'Keywords': response.css('meta[name*=keywords]::attr(content)').get(),
+                'Title': title,
+                'Category': category,
+                'Keywords': keywords,
                 'Publish date': response.css('.bread-crumb-detail__time p::text').get(),
                 'Cnt': self.cnt
             }
@@ -34,5 +54,21 @@ class QuotesSpider(scrapy.Spider):
             if self.cnt >= self.pageLimit:
                 return
             yield response.follow(nextUrl, callback=self.parse)
+
+
+def getSubjectName(category, keywords, title):
+    switcher = {
+        'Giáo dục': '__giao-duc__', 'Thời sự': '__thoi-su__', 'Kinh Doanh': '__kinh-doanh__',
+        'Thế giới': '__the-gioi__', 'Giải trí': '__giai-tri__', 'Đời sống': '__doi-song__',
+        'Pháp luật': '__phap-luat__', 'Thể thao': '__the-thao__',
+        'Thông tin và Truyền thông': '__thong-tin-truyen-thong__',
+        'Ô tô - Xe máy': '__oto-xe-may__', 'Bất động sản': '__bat-dong-san__', 'Bạn đọc': '__ban-doc__',
+        'Du lịch': '__du-lich__', 'Sức khỏe': '__suc-khoe__', 'Dân tộc - Tôn giáo': '__dan-toc-ton-giao__',
+        'Thị trường - tiêu dùng': '__thi-truong-tieu-dung__'
+    }
+    if category not in switcher.keys():
+        with open('scrapy_test/spiders/Output/unlabeledTrainSet.txt', 'a+', encoding='utf-8') as f:
+            f.write(title + '\n')
+    return switcher.get(category, '__' + category + keywords + '__')
 
 
